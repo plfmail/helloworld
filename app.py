bafc9e68171ad328c46cc569d643f3f056028fc7
plfmail <plfmail@sina.com>
2026-06-20 12:12:28 +0800

    Update Jenkinsfile.lab: Nexus password and network settings

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ def index():
 
 
 if __name__ == '__main__':
-    host = os.environ.get('HOST', '127.0.0.1')
+    host = os.environ.get('HOST', '0.0.0.0')
     port = int(os.environ.get('PORT', '5008'))
     app.run(host=host, port=port)
